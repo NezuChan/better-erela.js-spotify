2dd2a427f8c375267dabbc2e35a374b2ca2b850d
KagChi <KagChi@users.noreply.github.com>
2021-08-21 06:16:12 +0000

    chore(build): from b5b6d00e63a99f31be8844f9ab561b6e073f6b39

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,1 @@
-import { Spotify } from "./plugin";
-export = Spotify;
+export { Spotify as default } from "./plugin";
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
-const plugin_1 = require("./plugin");
-module.exports = plugin_1.Spotify;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = void 0;
+var plugin_1 = require("./plugin");
+Object.defineProperty(exports, "default", { enumerable: true, get: function () { return plugin_1.Spotify; } });
 //# sourceMappingURL=index.js.map
