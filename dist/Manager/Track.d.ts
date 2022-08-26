385050c7fcf1e03415b5144f0d4885a0b7c377da
KagChi <59391215+KagChi@users.noreply.github.com>
2022-08-26 23:51:53 +0700

    fix: cannot import plugin (#130)

--- dist/Manager/Track.d.ts
+++
@@ -1,5 +0,0 @@
-import { SearchResult } from "erela.js";
-import { BaseManager } from "./BaseManager";
-export declare class TrackManager extends BaseManager {
-    fetch(id: string, requester: unknown): Promise<SearchResult>;
-}
--- dist/Manager/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { TrackManager } from "./Track";
-import { PlaylistManager } from "./Playlist";
-import { ShowManager } from "./Show";
-import { EpisodeManager } from "./Episode";
-import { AlbumManager } from "./Album";
-import { ArtistManager } from "./Artist";
-export { PlaylistManager, EpisodeManager, TrackManager, ShowManager, AlbumManager, ArtistManager };
