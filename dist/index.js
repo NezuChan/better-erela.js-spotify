0ac07d6f214e3e376e0a41d6f608a75a3409deee
KagChi <59391215+KagChi@users.noreply.github.com>
2022-08-26 23:47:55 +0700

    chore: delete dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-var plugin_1 = require("./plugin");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return plugin_1.Spotify; } });
-//# sourceMappingURL=index.js.map
